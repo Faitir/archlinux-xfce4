9b20405e3a780f9f6888c94cfa132b254bf9b2ae
Faitir <67608799+Faitir@users.noreply.github.com>
2020-11-07 10:22:15 +0900

    Add files via upload

---
+++ arch-pkg-install.py
@@ -0,0 +1,18 @@
+import os
+#
+# wget file-roller unzip unrar parted reflector pulseaudio pavucontrol neofetch aspell-ru p7zip alsa-utils f2fs-tools
+#
+#os.system ( 'sudo pacman -S wget file-roller unzip unrar zip parted reflector pulseaudio pavucontrol neofetch p7zip alsa-utils dosfstools f2fs-tools' )
+#Утилиты
+#
+#
+# pycharm *gnome-terminal-transparency ( AUR )* *spotify ( snap )* guake *visual-studio-code-bin ( AUR )* telegram discord, chromium, plank
+# vim filezilla qbittorrent firefox gnome-calculator
+#
+#os.system ( ' ' )
+#Приложения
+#
+#
+#os.system ( 'sudo pacman -S libreoffice-still-ru' )
+#os.system ( 'hunspell-ru' )
+#LibreOffice  
