888ca66d05e6e8cb718b96eb230adeddd7aba077
Faitir <67608799+Faitir@users.noreply.github.com>
2020-11-08 15:41:49 +0900

    Update packages-xfce4.py

--- packages-xfce4.py
+++ packages-xfce4.py
@@ -1,5 +1,14 @@
 import os
-#Пакеты для ноутбука
-# xfce4-power-manager xfce4-battery-plugin xfce4-wavelan-plugin
+#Пакеты для ноутбука:
+# xfce4-power-manager, xfce4-battery-plugin, xfce4-wavelan-plugin
 #
-#os.system ( 'sudo pacman -S exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfconf xfwm4 xfce4-settings gnome-terminal xfdesktop xfce4-session gedit vlc shotwell thunar-archive-plugin thunar-media-tags-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-dict xfce4-diskperf-plugin xfce4-genmon-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xscreensaver xfce4-sensors-plugin xfce4-systemload-plugin xfce4-timer-plugin xfce4-verve-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin wget unzip unrar parted reflector pulseaudio pavucontrol neofetch aspell-ru p7zip alsa-utils f2fs-tools nvidia-settings ntfs-3g networkmanager plank gnome-calculator libreoffice-still-ru openssh' )
+#Разные пакеты:
+#   pacman:
+#       pycharm-community-edition, telegram-desktop, discord, filezilla, qbittorrent, guake, vim, [ tor torbrowser-launcher ], calibre, djview, krita, htop
+#   yay:
+#       visual-studio-code-bin, hunspell-ru, gnome-terminal-transparency, zoom
+#   snap:
+#       spotify
+#
+#
+#os.system ( 'sudo pacman -S exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfconf xfwm4 xfce4-settings gnome-terminal xfdesktop xfce4-session gedit vlc shotwell thunar-archive-plugin thunar-media-tags-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-dict xfce4-diskperf-plugin xfce4-genmon-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xscreensaver xfce4-sensors-plugin xfce4-systemload-plugin xfce4-timer-plugin xfce4-verve-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin wget unzip unrar parted reflector pulseaudio pavucontrol neofetch aspell-ru p7zip alsa-utils f2fs-tools nvidia-settings ntfs-3g networkmanager plank gnome-calculator libreoffice-still-ru openssh chromium firefox flameshot' )
